e40fe267295e5ab169a2e1a973519fa71935332d
Jon Sadowski <jwsadowski@gmail.com>
2022-11-10 16:32:28 -0600

    is it fixed

--- index.js
+++ index.js
@@ -6,42 +6,21 @@ inquirer
        name: 'id',
        message: 'What is your id?'
     },
+    {
+       name: 'email',
+       message: 'What is your email address?' 
+    },
+    {
+       name: 'github',
+       message: 'What is your github username?'
+    },
+    {
+       name: 'role',
+       message: 'What is your role?'
+    }
   ])
   .then(answers => {
-    console.info('Answer:', answers.id)
-  });
-
-inquirer
-  .prompt([
-    {
-        name: 'email',
-        message: 'What is your email address?'
-    },
-  ])
-  .then(answers => {
-    console.info('Answer:', answers.email)
-  });
-
-inquirer
-  .prompt([
-    {
-        name: 'github',
-        message: 'What is your github username?'
-    },
-  ]) 
-  .then(answers => {
-    console.info('Answer:', answers.github)
-  }) 
-
-inquirer
-  .prompt([
-    {
-        name: 'role',
-        message: 'What is your role?'
-    },
-  ])
-  .then(answers => {
-    console.info('Answer:,', answers.role)
+    console.info('Answers:', answers)
   }) 
 
 
